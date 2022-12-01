b504e251fa82178f1008ea0d597089cc2a9702c4
Dina <dina.olufsen@hotmail.com>
2022-12-01 17:56:51 +0100

    made function to split one string into small pieces in array for tags

---
+++ src/js/modules/splitStringToArray.js
@@ -0,0 +1,11 @@
+export function splitStringToArray(oneStringTags) {
+    if(oneStringTags){
+        const tagsArray = [];
+
+        const splitTags = oneStringTags.split(',');
+        splitTags.forEach((tag) => {tagsArray.push(tag.trim())
+        });
+        
+        return tagsArray;
+    } 
+}
