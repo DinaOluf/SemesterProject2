3f88e1e4dbefd33e4f416466d3933f7a5f652caa
Dina <dina.olufsen@hotmail.com>
2022-12-02 16:10:14 +0100

    improved date formatter

--- src/js/modules/timeDate.js
+++ src/js/modules/timeDate.js
@@ -2,23 +2,12 @@
  * @param {string | Date} date a date encoded string or date object
  * @returns {string} a formatted date string
  */
- export const formatDate = (date) => new Date(date).toLocaleString(("no-NO"), {
+ export const formatDate = (date) => new Date(date).toLocaleString(("en-ZA"), {
     weekday: 'long',
     year: 'numeric',
     month: 'short',
     day: 'numeric',
-  }) + ' at ' + new Date(date).toLocaleTimeString(("no-NO"), {
-    hour: 'numeric',
-    minute: 'numeric',
-  })
-
-  
-  export const todaysDate = () => new Date().toLocaleString(("no-NO"), {
-    weekday: 'short',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  }) + ' at ' + new Date().toLocaleTimeString(("no-NO"), {
+  }) + ' at ' + new Date(date).toLocaleTimeString(("en-ZA"), {
     hour: 'numeric',
     minute: 'numeric',
   })
