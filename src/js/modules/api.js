687320f4fd50a57e1349c769bac493d8efb3b4c6
Dina <dina.olufsen@hotmail.com>
2022-12-05 16:38:54 +0100

    Temporary notes to self in API file

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -5,5 +5,5 @@ export const LOGIN_URL = `${API_URL}/api/v1/auction/auth/login`;
 export const LISTINGS_URL = `${API_URL}/api/v1/auction/listings?sort=created&sortOrder=desc&_seller=true`;
 
 export const LISTING_URL = `${API_URL}/api/v1/auction/listings/`;
-//<ID>?_seller=true&_bids=true
-//<ID>/bids
+//<ID>?_seller=true&_bids=true&_active=true
+//<ID>/bids  -- to bid on listing
